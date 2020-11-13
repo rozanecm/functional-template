000c32cb7e41234b43a1ce9c701e48f8ee779261
Matias Rozanec <rozanecm@gmail.com>
2020-11-13 12:24:07 -0300

    add script to run all tests with just one cmd

---
+++ run_all_tests.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+lein test test/exercise1_test.clj
+lein test test/exercise2_test.clj
+lein test test/exercise3_test.clj
+lein test test/exercise4_test.clj
+lein test test/exercise5_test.clj
+lein test test/exercise6_test.clj
+lein test test/exercise7_test.clj
+lein test test/exercise8_test.clj
+lein test test/exercise9_test.clj
+lein test test/exercise10_test.clj
