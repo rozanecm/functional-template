f0dadd124a453ee4e3f530ff577cbd05ffc662c5
Matias Rozanec <rozanecm@gmail.com>
2020-11-23 20:27:31 -0300

    solve ex3

--- src/exercise3.clj
+++ src/exercise3.clj
@@ -3,7 +3,11 @@
 (defn fibonacci
   "Calculates the fibonacci result fo the given input value."
   [x]
-  (throw (Exception. "Not implemented."))
+  (case x
+   0 0
+   1 1
+   (+ (fibonacci (- x 1)) (fibonacci (- x 2)))
+  )
 )
 
 
