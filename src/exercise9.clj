4dc64962fb32ee058abb3095313ab755c48de07f
Matias Rozanec <rozanecm@gmail.com>
2020-11-23 22:40:34 -0300

    solve ex9

--- src/exercise9.clj
+++ src/exercise9.clj
@@ -1,9 +1,10 @@
 (ns exercise9)
+(require '[clojure.string :as s])
 
 (defn word-count 
   "Returns a map where each key is a distinct word in a given sentence and each value is the number of occurences of that word in the sentence."
   [x]
-  (throw (Exception. "Not implemented."))
+  (frequencies (s/split (s/lower-case x) #"\W+"))
 )
 
 
